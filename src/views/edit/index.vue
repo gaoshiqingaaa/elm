141df1aaecdfe00be5816ba99f8ecfed3c7d9669
liang0502 <67859152+liang0502@users.noreply.github.com>
2020-07-22 14:32:24 +0800

    Update index.vue

--- src/views/edit/index.vue
+++ src/views/edit/index.vue
@@ -1,5 +1,24 @@
 <template>
-    <div>
-        这是文本编辑
+    <div class="markdown">
+        <mavon-editor
+        style="min-height: 100%"
+        />
     </div>
 </template>
+
+<script>
+import { mavonEditor } from 'mavon-editor'
+import 'mavon-editor/dist/css/index.css'
+export default {
+    components:{
+        mavonEditor
+    },
+}
+</script>
+
+<style scoped>
+    .markdown{
+        width: 100%;
+        height: 100%;
+    }
+</style>
