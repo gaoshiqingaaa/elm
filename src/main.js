74065adc0bba18282b342bd419a73e1a17da62f7
apeic <648870158@qq.com>
2020-07-22 14:33:46 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,12 +3,10 @@ import App from './App.vue'
 import router from './router/index'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import myCharts from '@/utils/myCharts'
 
 Vue.config.productionTip = false
 
 Vue.use(ElementUI)
-Vue.use(myCharts)
 
 new Vue({
   router,
