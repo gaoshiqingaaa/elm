ee8785d1907292678870c5b5ac115450b11f4e7e
Geek <58128068+GeekWilliam@users.noreply.github.com>
2020-07-22 15:05:34 +0800

    Update index.vue

--- src/views/shop/index.vue
+++ src/views/shop/index.vue
@@ -1,56 +1,79 @@
 <template>
   <el-table
     :data="tableData"
-    stripe
+    border
     style="width: 100%">
     <el-table-column
+      fixed
       prop="name"
       label="店铺名称"
-      width="250">
+      width="210">
     </el-table-column>
     <el-table-column
       prop="address"
       label="店铺地址"
-      width="500">
+      width="210">
     </el-table-column>
     <el-table-column
       prop="category"
       label="店铺介绍"
-      width="300">
+      width="210">
     </el-table-column>
-     <el-table-column
+    <el-table-column
       fixed="right"
-      label="操作">
+      label="操作"
+      width="260">
       <template slot-scope="scope">
-        <el-button @click="handleClick(scope.row)" type="text" size="small">编辑</el-button>
-        <el-button type="text" size="small">添加食品</el-button>
-        <el-button type="text" size="small">删除</el-button>
+        <!-- <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
+        <el-button type="text" size="small">编辑</el-button> -->
+        <!-- <el-row> -->
+        <el-button @click="handleClick(scope.row)" size="small">编辑</el-button>
+        <el-button @click="handleClick(scope.row)" size="small">添加食品</el-button>
+        <el-button @click.native.prevent="deleteRow(scope.$index, tableData)" type="danger" size="small">删除</el-button>
+<!-- </el-row> -->
       </template>
     </el-table-column>
   </el-table>
 </template>
 
 <script>
-import {shopList} from '@/api/index'
+import {shoppingList} from '@/api/index'
   export default {
     data() {
       return {
         tableData: [],
-        params: {
-            latitude: 31.22967,
-            longitude: 121.4762
-        }
+        latitude:31.22967,
+        longitude:121.4762
+        
       };
-    },mounted(){
-        this.getList();
     },
-    methods: {
-        getList() {
-            shopList(this.params).then(res=>{
-                this.tableData = res.data
-                console.log(res)
-            })
-        }
+     mounted(){
+      this.getList();
+      
+    },
+    methods:{
+      getList(){
+        shoppingList(this.latitude,this.longitude).then(res=>{
+          this.tableData = res.data
+          console.log(res)
+        })
+      },
+      open() {
+        this.$alert('删除后不可恢复，确定吗？', '警告', {
+          confirmButtonText: '确定',
+          callback: action => {
+            this.$message({
+              type: 'info',
+              message: `action: ${ action }`
+            });
+          }
+          
+        });
+      
+      },
+      deleteRow(index, rows) {
+          rows.splice(index, 1);  
+          }
     }
   }
 </script>
