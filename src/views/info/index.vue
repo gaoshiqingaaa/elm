6748cc2cab0edd15e33f7157db38d67910d60332
liang0502 <67859152+liang0502@users.noreply.github.com>
2020-07-22 14:32:28 +0800

    Update index.vue

--- src/views/info/index.vue
+++ src/views/info/index.vue
@@ -1,5 +1,29 @@
 <template>
-    <div>
-        这是说明
+    <div class="readme">
+        
+        <p>node-elm后台管理系统</p>
+        
+        <p>第一次登录的用户自动注册成为普通管理员</p>
+                
+        <p>普通管理员可以添加，修改信息</p>
+        
+        <p>超级管理员可以删除信息</p>
     </div>
 </template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    .readme {
+        width: 100%;
+        height: 100%;
+        color: #666;
+        font: 400 20px/40px "微软雅黑";
+        text-align: center;
+        overflow: hidden;
+    }
+</style>
